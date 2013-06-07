d5fd16617f169d107f7927eb85d891ae4a058343
Vlad Orlenko <vlad@bjola.ca>
2013-06-07 00:07:38 -0700

    Preparing for first deployment

--- runtests.py
+++ runtests.py
@@ -4,11 +4,11 @@ def runtests():
     import os, sys, shutil, atexit
     from mezzanine.utils.importing import path_for_import
 
-    os.environ["DJANGO_SETTINGS_MODULE"] = "project_template.settings"
+    os.environ["DJANGO_SETTINGS_MODULE"] = "sfpirgapp.settings"
     mezz_path = path_for_import("mezzanine")
     sys.path.insert(0, mezz_path)
 
-    project_path = os.path.join(mezz_path, "project_template")
+    project_path = os.path.join(mezz_path, "sfpirgapp")
     local_settings_path = os.path.join(project_path, "local_settings.py")
     if not os.path.exists(local_settings_path):
         shutil.copy(local_settings_path + ".template", local_settings_path)
