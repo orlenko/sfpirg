73b38d8722ad0349c64dfe06b7ccf3d38f32b456
Vlad Orlenko <vlad@bjola.ca>
2013-10-17 23:18:45 -0700

    fixed filter in categ

--- sfpirgapp/views/category.py
+++ sfpirgapp/views/category.py
@@ -31,9 +31,9 @@ def get_ag_query_set(request, category):
         if user.is_superuser:
             return category.action_groups.all()
         else:
-            return category.action_groups.filter(Q(is_approved=True, is_underway=False) | Q(user=user))
+            return category.action_groups.filter(Q(is_approved=True) | Q(user=user))
     else:
-        return category.action_groups.filter(is_approved=True, is_underway=False)
+        return category.action_groups.filter(is_approved=True)
 
 
 def category(request, slug):
