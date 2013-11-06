52be85ea2481815ea8a2e8f38d6130b4ee8ed555
Vlad Orlenko <vlad@bjola.ca>
2013-11-06 09:50:12 -0800

    Updated ARX creation

--- sfpirgapp/views/arx.py
+++ sfpirgapp/views/arx.py
@@ -170,7 +170,7 @@ def create(request):
         liaison = Liaison.objects.create(name=(user.get_full_name() or user.username),
                                          email=user.email,
                                          organization=organization)
-        initial = {'liaison': liaison}
+        initial['liaison'] = liaison
     cat = _category_by_model(Project)
     initial['category'] = cat
     form = ProjectForm(request.POST or None, request.FILES or None, initial=initial)
